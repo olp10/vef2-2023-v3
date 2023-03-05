1af449159d3fdb4b1cc2c72a82d20cb6b8c3e939
Oli <olp10@hi.is>
2023-03-05 17:26:14 +0000

    Added @types/pg

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(router);
 app.use(departmentsRouter);
 app.use(classesRouter);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
